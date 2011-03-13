2bd4762dd95b6f4d85024a61b6ec38358a81d233
Mike Avello <info@mikeavello.com>
2011-03-13 17:27:11 -0500

    Cleaned up code and added overflow property

--- jquery.matchHeights.js
+++ jquery.matchHeights.js
@@ -1,17 +1,19 @@
 /*
  * Match Heights Plugin
- * Match the heights of targeted elements
+ * Match the heights of selected elements
  * 
- * Version 1.3
- * Updated 4/7/2010
- * Copyright (c) 2010 Mike Avello
+ * Version 1.6
+ * Updated 1/6/2011
+ * Copyright (c) 2010-2011 Mike Avello
  * Dual licensed under the MIT and GPL licenses:
  * http://www.opensource.org/licenses/mit-license.php
  * http://www.gnu.org/licenses/gpl.html
  *
- * Usage: $(object).matchHeights({
- *		minHeight: [optional],
- *		maxHeight: [optional]
+ * Usage: 
+ * $(object).matchHeights({
+ *		minHeight: null, // optional minimum height setting
+ *		maxHeight: null, // optional maximum height setting, forced height instead of min-height
+ *		overflow: false // sets overflow to be hidden. Default is false; overflow attribute not set
  * });
  *
  */
@@ -19,7 +21,8 @@
 	$.fn.matchHeights = function(settings) {
 		settings = jQuery.extend(this,{
 			minHeight: null, // optional minimum height setting
-			maxHeight: null // optional maximum height setting, forced height instead of min-height
+			maxHeight: null, // optional maximum height setting, forced height instead of min-height
+			overflow: false // sets overflow to be hidden. Default is false; overflow attribute not set
 		}, settings);
 	
 		tallest = (settings.minHeight) ? settings.minHeight : 0;
@@ -30,12 +33,9 @@
 		});
 		if((settings.maxHeight) && tallest > settings.maxHeight) tallest = settings.maxHeight;
 		return this.each(function() {
-			extra = $(this).innerHeight() - $(this).height();
-			
-			//if ($(this).outerHeight() - $(this).innerHeight() == 0)	
-				extra = extra + ($(this).outerHeight() - $(this).innerHeight());
-			
-			($.browser.msie && $.browser.version == 6.0 || (settings.maxHeight)) ? $(this).css({'height': tallest - extra}) : $(this).css({'min-height': tallest - extra}); 
+			padding = $(this).innerHeight() - $(this).height();
+			extra = padding + ($(this).outerHeight() - $(this).innerHeight());
+			($.browser.msie && $.browser.version == 6.0 || (!settings.overflow == false)) ? $(this).css({'height': tallest - extra, 'overflow': settings.overflow}) : $(this).css({'min-height': tallest - extra}); 
 		});
 	}
 })(jQuery);
