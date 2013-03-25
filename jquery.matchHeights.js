5af1c3bb67e07b4ab173f510facd30c954fe8914
Mike Avello <info@mikeavello.com>
2013-03-25 13:29:20 -0500

    Added "includeMargin" option and increased version number
    new "includeMargin" option allows for element margins to be included in
    the calculated values when being matched.

--- jquery.matchHeights.js
+++ jquery.matchHeights.js
@@ -1,8 +1,8 @@
 /*
  * Match Heights jQuery Plugin
  * 
- * Version 1.7alpha (Updated 5/30/2012)
- * Copyright (c) 2010-2012 Mike Avello
+ * Version 1.7.1 (Updated 3/25/2013)
+ * Copyright (c) 2010-2013 Mike Avello
  * Dual licensed under the MIT and GPL licenses:
  * http://www.opensource.org/licenses/mit-license.php
  * http://www.gnu.org/licenses/gpl.html
@@ -11,10 +11,11 @@
 (function($) {
 	$.fn.matchHeights = function(settings) {
 		settings = jQuery.extend(this,{
-			minHeight: null,	// optional minimum height setting
-			maxHeight: null,	// optional maximum height setting, forced height instead of min-height
-			extension: 0,		// optional amount to add to calculated height
-			overflow: false		// optional setting for overflow. Default is false; overflow attribute not set
+			minHeight: null,			// Optional minimum height setting
+			maxHeight: null,			// Optional maximum height setting, forced height instead of min-height
+			extension: 0,				// Optional amount to add to calculated height
+			overflow: false,			// Option to enable overflow. Default is false; overflow attribute not set
+			includeMargin: false	// Optional Setting to include margin within calculations for alignment. Default is false.
 		}, settings);
 		
 		var extension = settings.extension;
@@ -31,7 +32,7 @@
 		return this.each(function() {
 			var element = $(this);
 			var padding = element.innerHeight() - element.height();
-			var extra = padding + ( element.outerHeight() - element.innerHeight() );
+			var extra = padding + ( element.outerHeight( settings.includeMargin ) - element.innerHeight() );
 			if ( $.browser.msie && $.browser.version == 6.0 || (!settings.overflow == false) ) {
 				element.css({'height': tallest - extra + extension, 'overflow': settings.overflow});
 			} else {
